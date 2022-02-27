7ea36e227e642f5f1701c548eccda06a8b80d2ba
Sharon Shaji <2259122s@student.gla.ac.uk>
2022-02-27 18:06:15 +0000

    Checking my setup

--- festival/views.py
+++ festival/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse("Welcome to shareFestival!")
