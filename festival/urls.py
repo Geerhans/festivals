e2d28afdba2fa509543194955d29bab5eeba2da3
Sharon Shaji <2259122s@student.gla.ac.uk>
2022-03-02 23:30:32 +0000

    Updated urls.py

--- festival/urls.py
+++ festival/urls.py
@@ -4,5 +4,14 @@ from festival import views
 app_name = 'shareFestival'
 
 urlpatterns = [
-path('', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('festivalHistory/', views.festivalHistory, name='festivalHistory'),
+    path('shareStory/', views.shareStory, name='shareStory'),
+    path('personalCenter/', views.personalCenter, name='personalCenter'),
+    path('shareStory/', views.shareStory, name='shareStory'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.user_logout, name='user_logout'),
+    path('signup/', views.signUp, name='signUp'),
+    path('contactUs/', views.contactUs, name='contactUs'),
 ]
