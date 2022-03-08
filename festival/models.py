dc8729b0a40890f98ed82f4eea1af4384e413820
Xinwei Duan <2650178D@student.gla.ac.uk>
2022-03-08 20:28:25 +0000

    update models

--- festival/models.py
+++ festival/models.py
@@ -1,23 +1,14 @@
 from operator import mod
 from django.db import models
 from django.utils import timezone
-from django.template.defaultfilters import slugify
 
-from django.utils import timezone
 
 # Create your models here.
-#class User(models.Model):
-
 
 class Country(models.Model):
-
-    countryID = models.IntegerField(max_length=128, unique=True)
-    countryname = models.CharField(max_length=128)
-    slug = models.SlugField()
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.countryname) 
-        super(Country, self).save(*args, **kwargs)
-
+    countryID = models.IntegerField(max_length=128,default=0)
+    countryname = models.CharField(max_length=128, unique=True)
+    
     class Meta:
         verbose_name_plural = 'Countries'
     
@@ -27,17 +18,11 @@ class Country(models.Model):
     
 
 class Festival(models.Model):
-
-    festivalID = models.IntegerField(max_length=128, unique=True)
-    countryID = models.ManyToManyField(Country, on_delete=models.CASCADE)
+    countryname = models.ForeignKey(Country, on_delete=models.CASCADE)
+    festivalID = models.IntegerField(max_length=128,default=0)
     festivalname = models.CharField(max_length=128)
     body = models.TextField()
     #image = models.ImageField()
-    views = models.IntegerField(default=0)
-    slug = models.SlugField()
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.festivalname) 
-        super(Festival, self).save(*args, **kwargs)
 
     
     class Meta:
@@ -80,4 +65,3 @@ class Comment(models.Model):
         verbose_name_plural = 'Comments'
     def __str__(self):
         return self.body[:20]
-
