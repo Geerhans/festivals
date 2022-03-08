01eefdfd99b6678cea81de985189a410ef20a1f9
Geer Han <2640382h@student.gla.ac.uk>
2022-03-08 22:29:17 +0000

    change admin

--- festival/admin.py
+++ festival/admin.py
@@ -3,9 +3,10 @@ from django.contrib import admin
 
 from festival.models import Country, Story, Festival, Comment
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('countryname',)}
 
-
-admin.site.register(Country)
+admin.site.register(Country, CategoryAdmin)
 admin.site.register(Story)
 admin.site.register(Festival)
 admin.site.register(Comment)
