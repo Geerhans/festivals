3bd46196cb4e7d9b1d7e7f9608426006ef902c21
Sharon Shaji <2259122s@student.gla.ac.uk>
2022-03-08 21:58:46 +0000

    Added cookie handler

--- festival/views.py
+++ festival/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect
 from django.urls import reverse
 from festival.models import Country, Festival
+from datetime import datetime
 
 
 # Create your views here.
@@ -10,16 +11,21 @@ from django.http import HttpResponse
 def index(request):
  # 1. Query the database for a list of ALL countries currently stored.
  # 2. Order the countries by the number of views in ascending order.
-    #country_list = Country.objects.order_by('views')[:5]
+    country_list = Country.objects.order_by('views')[:5]
     context_dict = {}
-    #context_dict['countries'] = country_list
+    context_dict['countries'] = country_list
+
+    visitor_cookie_handler(request)
 
     #return HttpResponse("Welcome to shareFestival!")
     response = render(request, 'festival/index.html', context=context_dict)
     return response
 
 def festivalHistory(request):
-    return render(request, 'festival/festivalHistory.html')
+    context_dict = {}
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    return render(request, 'festival/festivalHistory.html', context=context_dict)
 
 def shareStory(request):
     return render(request, 'festival/shareStory.html')
@@ -42,7 +48,7 @@ def Login(request):
             else:
                 return HttpResponse("Your shareStory account is disabled.")
         else:
-            print(f"Invalid login details: {username}, {password}")
+            print("Invalid login details: {username}, {password}")
             return HttpResponse("Invalid login details supplied.")
     else:
         return render(request, 'festival/Login.html')
@@ -61,16 +67,65 @@ def contactUs(request):
     return render(request, 'festival/contactUs.html')
 
 def India(request):
-    return render(request, 'festival/india.html')
+    context_dict = {}
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    return render(request, 'festival/india.html', context=context_dict)
 
 def UK(request):
-    return render(request, 'festival/UK.html')
+    context_dict = {}
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    return render(request, 'festival/UK.html', context=context_dict)
 
 def USA(request):
-    return render(request, 'festival/USA.html')
+    context_dict = {}
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    return render(request, 'festival/USA.html', context=context_dict)
 
 def Italy(request):
-    return render(request, 'festival/italy.html')
+    context_dict = {}
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    return render(request, 'festival/italy.html', context=context_dict)
 
 def China(request):
-    return render(request, 'festival/china.html')
+    context_dict = {}
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    return render(request, 'festival/china.html', context=context_dict)
+
+#Handle cookies
+def visitor_cookie_handler(request, response):
+    visits = int(request.COOKIES.get('visits', '1'))
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+    '%Y-%m-%d %H:%M:%S')
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        response.set_cookie('last_visit', str(datetime.now()))
+    else:
+        response.set_cookie('last_visit', last_visit_cookie)
+    response.set_cookie('visits', visits)
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request,
+    'last_visit',
+    str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+    '%Y-%m-%d %H:%M:%S')
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+        
+    request.session['visits'] = visits
