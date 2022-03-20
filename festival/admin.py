4c6846e46c3c6f4e42d649dc7d9ea851630ee8e9
Xinwei Duan <2650178D@student.gla.ac.uk>
2022-03-20 09:45:20 +0000

    delete useless code

--- festival/admin.py
+++ festival/admin.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
 
 from festival.models import UserProfile
-from festival.models import Country, Story, Festival, Comment
+from festival.models import Country,Festival
 # Register your models here.
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('countryname',)}
 
 admin.site.register(Country, CategoryAdmin)
-admin.site.register(Story)
+
 admin.site.register(Festival)
-admin.site.register(Comment)
+
 admin.site.register(UserProfile)
 
 
