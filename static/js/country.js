795980d44aa83cf4ce2fd847b950cefd268fd967
Peiyu Jiang <2704229j@student.gla.ac.uk>
2022-03-20 00:30:13 +0000

    js

---
+++ static/js/country.js
@@ -0,0 +1,5 @@
+function jump_to(country){
+    console.log(window.location.href)
+    const part_url=window.location.href.substring(0,window.location.href.indexOf("country"))
+    window.location.href = part_url + "country/" + country +"/"
+}
